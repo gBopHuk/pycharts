f55e2d8f94de8eab82c8374347535e180f55a672
gBopHuk <gbophuk_alt@mail.ru>
2014-01-07 12:28:00 +0400

    jhkjlk

--- preparam.py
+++ preparam.py
@@ -1,14 +1,15 @@
 import sys
 
-filename = sys.argv[1]
-csvf = open(filename.decode('utf-8')
-i=0
+#filename = sys.argv[1]
 
-for line in csv.readlines():
-	sline = line.split(',')
-	dtime[i] = sline[0]+' '+sline[1]
-	fopen[i],fhigh[i],flow[i],fclose[i]=float(sline[2]),\
-									float(sline[3]),\
-									float(sline[4]),\
-									float(sline[5])
-	i++
+def getparam(filename)
+	csvf = open(filename.decode('utf-8')
+	i=0
+	for line in csvf.readlines():
+		sline = line.split(',')
+		dtime[i] = sline[0]+' '+sline[1]
+		fopen[i],fhigh[i],flow[i],fclose[i]=float(sline[2]),\
+										float(sline[3]),\
+										float(sline[4]),\
+										float(sline[5])
+		i++
